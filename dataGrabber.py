82247a768f579a5644e27abb98ce0c27be01c0e5
DESKTOP-TBQ6K2I\USER <michael.yen@varmeego.com>
2021-09-02 15:25:11 +0800

    [UPDATE]

--- dataGrabber.py
+++ dataGrabber.py
@@ -11,6 +11,9 @@ import json
 import argparse
 import os
 from io import StringIO
+from pathlib import Path
+import schedule
+import pytz
 import time
 
 from utils import (dict_to_dataFrame, add_columns_between_two_dataFrames,
@@ -64,7 +67,7 @@ def get_data_by_api(api_name, url, access_token, payload):
 
     if response.status_code == 200:
         logger.info(f'get {api_name} data successfully')
-        df = pd.read_json(StringIO(response.text), lines=True)
+        df = pd.read_json(StringIO(response.text), lines=True, nrows=10)
         return df
     else:
         logger.error(f'get {api_name} data failed. response code: {response.status_code}')
@@ -72,88 +75,119 @@ def get_data_by_api(api_name, url, access_token, payload):
 
 def get_public_sales_data(args, access_token, output_dir, save_filename, dl_filename, zip_files):
     """Get public sales data"""
-    public_sales_link = public_sales_api(args['url'], access_token, "2021-06-23", "2021-06-24")  # ytd.strftime("%Y-%m-%d"), td.strftime("%Y-%m-%d")
-    if public_sales_link:
-        csv_gz_path = download_by_url(public_sales_link, output_dir, f'{dl_filename}.gz')
-        if csv_gz_path:
-            download_dataFrame = pd.read_csv(csv_gz_path, compression='gzip', error_bad_lines=False)
-            column_name_array = ['membership_level', 'device_type', 'card_type', 'housing_type', 'order_value', 'total_discounts', 'sku_id', 'sku_name_chi', 'brand_chi', 'quantity', 'unit_price', 'primary_category', 'primary_category_name_chi', 'sub_cat_1_name_chi', 'sub_cat_2_name_chi', 'sub_cat_3_name_chi', 'order_sku_comm_rate', 'order_sku_comm_amount', 'sku_level_promotion_amount']
-            df = add_columns_between_two_dataFrames(download_dataFrame, column_name_array)
-            dataFrame_to_csv(df, output_dir, 'Public_Transaction_' + save_filename + '.csv')
-            zip_files.append(f'{output_dir}/Public_Transaction_{save_filename}.csv')
-            return zip_files
-
-def get_general_data(payload, url, access_token, api_name, output_dir, save_filename, zip_files):
-    data = get_data_by_api(api_name, url, access_token, payload)
-    if data is not None:
-        dataFrame_to_csv(data, output_dir, f'{api_name}_{save_filename}.csv')
-        zip_files.append(f'{output_dir}/{api_name}_{save_filename}.csv')
+    file_path = Path(f'{output_dir}/Public_Transaction_{save_filename}.csv')
+    if file_path.is_file():
+        logger.info(f'Public_Transaction has already existed')
+        zip_files.append(f'{output_dir}/Public_Transaction_{save_filename}.csv')
+    else:
+        public_sales_link = public_sales_api(args['url'], access_token, "2021-06-23", "2021-06-24")  # ytd.strftime("%Y-%m-%d"), td.strftime("%Y-%m-%d")
+        if public_sales_link:
+            csv_gz_path = download_by_url(public_sales_link, output_dir, f'{dl_filename}.gz')
+            if csv_gz_path:
+                download_dataFrame = pd.read_csv(csv_gz_path, compression='gzip', error_bad_lines=False)
+                column_name_array = ['membership_level', 'device_type', 'card_type', 'housing_type', 'order_value', 'total_discounts', 'sku_id', 'sku_name_chi', 'brand_chi', 'quantity', 'unit_price', 'primary_category', 'primary_category_name_chi', 'sub_cat_1_name_chi', 'sub_cat_2_name_chi', 'sub_cat_3_name_chi', 'order_sku_comm_rate', 'order_sku_comm_amount', 'sku_level_promotion_amount']
+                df = add_columns_between_two_dataFrames(download_dataFrame, column_name_array)
+                dataFrame_to_csv(df, output_dir, f'Public_Transaction_{save_filename}.csv')
+                zip_files.append(f'{output_dir}/Public_Transaction_{save_filename}.csv')
     return zip_files
 
-def main(args, logger):
-    while True:
-        td = datetime.date.today()
-        ytd = td - datetime.timedelta(days=1)
+def get_general_data(payload, url, access_token, api_name, output_dir, save_filename, zip_files):
+    file_path = Path(f'{output_dir}/{api_name}_{save_filename}.csv')
+    if file_path.is_file():
+        logger.info(f'{api_name} has already existed')
+        zip_files.append(f'{output_dir}/{api_name}_{save_filename}.csv')
+    else:
+        data = get_data_by_api(api_name, url, access_token, payload)
+        if data is not None:
+            dataFrame_to_csv(data, output_dir, f'{api_name}_{save_filename}.csv')
+            zip_files.append(f'{output_dir}/{api_name}_{save_filename}.csv')
+    return zip_files
+
+def scraper(args, logger):
+    timezone = pytz.timezone(args['timezone'])
+    start_time = datetime.datetime.now(timezone)
+
+    td = datetime.date.today()
+    ytd = td - datetime.timedelta(days=1)
+    
+    dl_filename = f'Public_Transaction_{ytd.strftime("%d%m%Y")}_{td.strftime("%d%m%Y")}.csv'
+    save_filename = f'{ytd.strftime("%d%m%Y")}_{td.strftime("%d%m%Y")}'
+    save_filename_1 = f'{ytd.strftime("%d%m%Y")}'
+    output_dir = f'{args["output_dir"]}/{ytd}-{td}'
+    zip_files = []
+    zipFile_dir = f'{output_dir}/{ytd}-{td}.zip'
+
+    username = args['username']
+    password = args['password']
+    sys_token = args['merchant_code']
+    sys_store_code = args['store_code'][0]
+    payload = {
+        'sys_email': username,
+        'sys_token': sys_token,
+        'sys_store_code': sys_store_code,
+    }
+
+    access_token = retrieve_access_token(logger, 'password', username, password)
+    if access_token:
+        zip_files = get_public_sales_data(args, access_token, output_dir, save_filename, dl_filename, zip_files)
+        # Get merchant user data
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantUser', access_token=access_token, api_name='Merchant_User', output_dir=output_dir, save_filename=save_filename, zip_files=zip_files)
+        # Get Merchant Product
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantProduct', access_token=access_token, api_name='Merchant_Product', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        # Get Merchant Transaction
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantSaleTransactionSimplified', access_token=access_token, api_name='Merchant_Sale_Transaction_Simplified', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        # Get Merchant Wishlistjd
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantWishlistItem', access_token=access_token, api_name='Merchant_WishlistItem', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)        
+        # Get Merchant Shared Shopping Carts
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantSharedCartEntry', access_token=access_token, api_name='Merchant_SharedCartEntry', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        # Get Merchant Online Stores
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantOnlineStore', access_token=access_token, api_name='Merchant_OnlineStore', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        # Get merchant website and app traffic product data
+        payload['date'] = ytd.strftime("%Y-%m-%d")
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantTrafficProduct', access_token=access_token, api_name='Merchant_Website_And_App_Traffic_Product', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        del payload['date']
+        # Get merchant website and app traffic product list data
+        payload['date'] = ytd.strftime("%Y-%m-%d")
+        zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantTrafficProductList', access_token=access_token, api_name='Merchant_Website_And_App_Traffic_ProductList', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
+        del payload['date']
+
+        # Zip files
+        zip_file_path = Path(f'{zipFile_dir}')
+        if zip_file_path.is_file():
+            logger.info(f'{zip_file_path} has already existed')
+        else:
+            logger.info('zipping files...')
+            zip_file(zip_files, zipFile_dir)
         
-        dl_filename = f'[ori]Public_Transaction_{ytd.strftime("%d%m%Y")}_{td.strftime("%d%m%Y")}.csv'
-        save_filename = f'{ytd.strftime("%d%m%Y")}_{td.strftime("%d%m%Y")}'
-        save_filename_1 = f'{ytd.strftime("%d%m%Y")}'
-        output_dir = f'{args["output_dir"]}/{ytd}-{td}'
-        zip_files = []
-        zipFile_dir = f'{output_dir}/{ytd}-{td}.zip'
+        # Remove files
+        for i, file in enumerate(zip_files):
+            os.remove(file)
+        os.remove(f'{output_dir}/{dl_filename}.gz')
+        
 
-        username = args['username']
-        password = args['password']
-        sys_token = args['merchant_code']
-        sys_store_code = args['store_code'][0]
-        payload = {
-            'sys_email': username,
-            'sys_token': sys_token,
-            'sys_store_code': sys_store_code,
-        }
+        # send via ftp
+        ftp_host = args['ftp_host']
+        ftp_username = args['ftp_username']
+        ftp_password = args['ftp_password']
+        ftp_output_dir = args['ftp_output_dir']
 
-        access_token = retrieve_access_token(logger, 'password', username, password)
-        if access_token:
-            zip_files = get_public_sales_data(args, access_token, output_dir, save_filename, dl_filename, zip_files)
-            # Get merchant user data
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantUser', access_token=access_token, api_name='Merchant_User', output_dir=output_dir, save_filename=save_filename, zip_files=zip_files)
-            # Get Merchant Product
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantProduct', access_token=access_token, api_name='Merchant_Product', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            # Get Merchant Transaction
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantSaleTransactionSimplified', access_token=access_token, api_name='Merchant_Sale_Transaction_Simplified', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            # Get Merchant Wishlistjd
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantWishlistItem', access_token=access_token, api_name='Merchant_WishlistItem', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)        
-            # Get Merchant Shared Shopping Carts
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantSharedCartEntry', access_token=access_token, api_name='Merchant_SharedCartEntry', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            # Get Merchant Online Stores
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantOnlineStore', access_token=access_token, api_name='Merchant_OnlineStore', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            # Get merchant website and app traffic product data
-            payload['date'].append(ytd.strftime("%Y-%m-%d"))
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantTrafficProduct', access_token=access_token, api_name='Merchant_Website_And_App_Traffic_Product', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            del payload['date']
-            # Get merchant website and app traffic product list data
-            payload['date'].append(ytd.strftime("%Y-%m-%d"))
-            zip_files = get_general_data(payload, url=args['url'] + '/gw/api/v1/MerchantTrafficProductList', access_token=access_token, api_name='Merchant_Website_And_App_Traffic_ProductList', output_dir=output_dir, save_filename=save_filename_1, zip_files=zip_files)
-            del payload['date']
+        logger.info(f'sending file to {ftp_host} via ftp')
+        send_ftp(ftp_host, ftp_username, ftp_password, zipFile_dir ,ftp_output_dir)
 
-            if os.path.exists(output_dir):
-                logger.info('zipping files')
-                zip_file(zip_files, zipFile_dir)
-                
-                # send via ftp
-                ftp_host = args['ftp_host']
-                ftp_username = args['ftp_username']
-                ftp_password = args['ftp_password']
-                ftp_output_dir = args['ftp_output_dir']
+        # Send via email
+        #logger.info(f'sending email to {args["receiver_email"]} from {args["sender_email"]}...')
+        #send_email(args['subject'], args['body'], args['sender_email'], args['receiver_email'], args['email_password'], args['stmp'], args['stmp_port'], zipFile_dir)
 
-                logger.info(f'sending file to {ftp_host} via ftp')
-                send_ftp(ftp_host, ftp_username, ftp_password, ftp_output_dir)
+        end_time = datetime.datetime.now(timezone)
+        logger.info(f'Finished. Total time: {end_time-start_time}')
 
-                # send via email
-                #logger.info(f'sending email to {args["receiver_email"]} from {args["sender_email"]}...')
-                #send_email(args['subject'], args['body'], args['sender_email'], args['receiver_email'], args['email_password'], args['stmp'], args['stmp_port'], zipFile_dir)
+def main(args, logger):
+    #schedule.every().day.at("17:26").do(scraper, args=args, logger=logger)
 
-                logger.info(f'finished')
+    #while True:
+    #    schedule.run_pending()
+    #    time.sleep(9)
+    scraper(args, logger)
 
 def _get_parser():
     parser = argparse.ArgumentParser(description='Automate data grabber from HKTVmall mms portal')
